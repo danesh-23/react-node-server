f4895a7c7818bace34b57d9521397d9131ab84b1
Danesh <daneshpbc@gmail.com>
2020-06-10 01:25:42 +0800

    Commit

---
+++ Server/build/precache-manifest.feb5f5a79055b5160e03333a1a1cfc39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e17572c6cca8b15f4bb309db598051a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe1ccc7782bf77a113c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fee3daae52e90ce3bb6",
+    "url": "/static/js/2.2d3ba5cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d3ba5cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe1ccc7782bf77a113c",
+    "url": "/static/js/main.83771db7.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const path = require("path")
 
 app.use(bodyParser.json())
 
@@ -8,6 +9,12 @@ app.use(bodyParser.json())
 app.get("/", (req, res) => {
     res.send({message: "hello, world"})
 })
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 app.get("/api/data", (req, res) => {
     res.send({
